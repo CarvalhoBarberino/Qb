867072996e90671eddf01fb33c96d14e15326d6d
CarvalhoBarberino <35077550+CarvalhoBarberino@users.noreply.github.com>
2018-06-29 00:48:31 -0300

    Add files via upload

--- Qb/src/Complexo.java
+++ Qb/src/Complexo.java
@@ -16,7 +16,7 @@ public class Complexo {
 		real = r;
 		imaginario = i;
 		norma = norma();
-		if(norma > 0) {
+		if(norma != 0) {
 			fase = Math.acos(real/norma);
 		}else {fase = 0;}
 	}
@@ -29,7 +29,7 @@ public class Complexo {
 		return(new Complexo((a.real*b.real - a.imaginario*b.imaginario), (a.real*b.imaginario + a.imaginario*b.real)));
 	}
 	//****
-	public void mostrarCartesiano() {
+	public void mostrar() {
 		if(imaginario >= 0) {
 			System.out.println(real + " + i*" + imaginario);
 		}else {
@@ -39,10 +39,37 @@ public class Complexo {
 	}
 	//****
 	public double norma() {
-		return(Math.sqrt(real*real+imaginario+imaginario));
+		return(Math.sqrt(real*real+imaginario*imaginario));
 	}
 	//****
-	public void mostraPolar() {
+	public void mostrarPolar() {
 		System.out.println(norma + " < " + fase*180/Math.PI);
+	}
+	//****
+	public void conjugado(){
+		imaginario = -imaginario;
+	}
+	//****
+	public Complexo conjugado(Complexo x){
+		return(new Complexo(x.real, -x.imaginario));
+	}
+	//****
+	public Complexo clone(){
+		return(new Complexo(real, imaginario));
+	}
+	//****
+	public static Complexo newPolar(double norma, double fase){
+		return(new Complexo(norma * Math.cos(fase), norma * Math.sin(fase)));
+	}
+	//****
+	public void setPolar(double normaArg, double faseArg){
+		real = normaArg * Math.cos(faseArg);
+		imaginario = normaArg * Math.sin(faseArg);
+		norma = normaArg;
+		fase = faseArg;
+	}
+	//****
+	public Complexo pow(double e){
+		return(Complexo.newPolar(Math.pow(norma, e), e * fase));
 	}
 }
--- Qb/src/Main.java
+++ Qb/src/Main.java
@@ -1,11 +1,19 @@
 
 public class Main {
 	public static void main(String args[]) {
-		Complexo x = new Complexo(0,1);
-		x.mostrarCartesiano();
-		x.mostraPolar();
-		Complexo z = Complexo.multiplicacao(x, x);
-		z.mostrarCartesiano();
-		z.mostraPolar();
+		System.out.println("Qual a raiz quadrada de -1?");
+		Complexo menos1 = new Complexo(-1, 0);
+		System.out.print("Resposta: ");
+		menos1.pow(0.5).mostrar();
+		System.out.println();
+		Complexo x = new Complexo(0.3, 0.4);
+		x.mostrar();
+		x.mostrarPolar();
+		Complexo y = Complexo.newPolar(2, 30*Math.PI/180);
+		y.mostrar();
+		y.mostrarPolar();
+		Complexo z = y.pow(0.5);
+		z.mostrar();
+		z.mostrarPolar();
 	}
 }
